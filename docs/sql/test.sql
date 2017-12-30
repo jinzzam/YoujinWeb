833a0818402a1917fee06d91cf4d954a43b0a1e0
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2017-12-30 15:27:00 +0900

    delete table & drop table

--- docs/sql/test.sql
+++ docs/sql/test.sql
@@ -16,6 +16,11 @@ drop table member;  #테이블 삭제
 
 insert into member (name, birthday, gender, id, password, yourword)
 values ('박유진', date('1997-11-19'), 'f', 'youjin', 'pass', '김태정=왑최고미남');	#열의 종류를 나열한 뒤에 값을 대입해줌
+
+insert into member 
+values ('김태정', date('1993-01-05'), 'm', 'kukaro', 'kukuku', '박유진=왑최고귀요미');
+
+update member set password = 'kakaka', gender = 'f' where id = 'kukaro';
 select * from member;	#member table을 조회
 
 show tables;  #테이블이 뭐 있는지 보여줌 (테이블 목록)
@@ -28,3 +33,5 @@ show variables where variable_name LIKE"%dir";	#mysql 설정파일 위치 찾기
 alter database mydb default character set utf8;	#database character set 설정
 show full columns from member;	#테이블 열 정보 표시
 alter table member convert to character set utf8;	# table character set 설정
+
+
